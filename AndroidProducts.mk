115ba30d2dedc9ffe6462a9f912a297e470d14ad
D4rkKnight21 <yusuff8621@gmail.com>
2021-09-21 20:36:20 +0700

    platina: bringup xdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_platina.mk
+    $(LOCAL_DIR)/xdroid_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_platina-user \
-    arrow_platina-userdebug \
-    arrow_platina-eng
+    xdroid_platina-user \
+    xdroid_platina-userdebug \
+    xdroid_platina-eng
--- xdroid_platina.mk
+++ xdroid_platina.mk
@@ -19,12 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common ArrowOs stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common xdroid-CAF stuff
+$(call inherit-product, vendor/xdroid/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-ARROW_BUILD_TYPE := DHAMPOT
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -32,12 +31,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_platina
+PRODUCT_NAME := xdroid_platina
 PRODUCT_MODEL := MI 8 Lite
 
-DEVICE_MAINTAINER := D4rkKnight21
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-google
 
 TARGET_VENDOR_PRODUCT_NAME := platina
 
