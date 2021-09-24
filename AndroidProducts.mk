09855cc0ec445f299157304f5ac8e59d7cd8bd2b
D4rkKnight21 <yusuff8621@gmail.com>
2021-09-24 08:48:58 +0700

    platina: Bringup Xperience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_platina.mk
+    $(LOCAL_DIR)/xperience_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_platina-user \
-    arrow_platina-userdebug \
-    arrow_platina-eng
+    xperience_platina-user \
+    xperience_platina-userdebug \
+    xperience_platina-eng
